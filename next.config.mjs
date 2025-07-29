325f58656fd16bfa969e115cb969ea4ef489de50
[resong84] <[resong84@gmail.com]>
2025-07-29 16:28:12 +0900

    minitest

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+const nextConfig = {};
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
 }
 
-export default nextConfig
+export default nextConfig;
